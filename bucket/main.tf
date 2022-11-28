ded563b0fbf488e5f3398fdb42e7d7f8ab16d57a
Bob Yuwono <bobywn@gmail.com>
2022-11-28 02:34:54 +0000

    fix bucket

--- bucket/main.tf
+++ bucket/main.tf
@@ -13,21 +13,8 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_storage_bucket" "static-site" {
-  name          = "image-store.com"
-  location      = "US"
-  force_destroy = true
-
-  uniform_bucket_level_access = true
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["http://image-store.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
+resource "google_storage_bucket" "default" {
+  name = var.bucket_name
+  storage_class = var.storage_class
+  location = var.bucket_location
 }
