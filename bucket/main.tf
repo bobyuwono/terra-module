76fc79ecf559315ad98b94b7fd6a7e7075a80b37
Bob Yuwono <bobywn@gmail.com>
2022-11-28 02:05:18 +0000

    fix bucket module

--- bucket/main.tf
+++ bucket/main.tf
@@ -13,6 +13,21 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+resource "google_storage_bucket" "static-site" {
+  name          = "image-store.com"
+  location      = "US"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
 }
