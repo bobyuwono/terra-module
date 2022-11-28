e2e4509be22296ad43aa09ede97ef786d8032423
Bob Yuwono <bobywn@gmail.com>
2022-11-28 02:51:56 +0000

    fix bucket

--- bucket/main.tf
+++ bucket/main.tf
@@ -7,12 +7,6 @@ terraform {
   }
 }
 
-provider "google" {
-  project = var.project
-  region  = var.region
-  zone    = var.zone
-}
-
 resource "google_storage_bucket" "default" {
   name = "var.bucket_name"
   storage_class = var.storage_class
